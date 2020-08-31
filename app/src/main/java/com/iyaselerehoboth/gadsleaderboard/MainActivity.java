0c62ca87f09e8e143558db4720c47613f446dc36
Rehoboth <rehobothi@yahoo.com>
2020-08-31 03:05:52 +0100

    Done with Splashscreen

--- app/src/main/java/com/iyaselerehoboth/gadsleaderboard/MainActivity.java
+++ app/src/main/java/com/iyaselerehoboth/gadsleaderboard/MainActivity.java
@@ -1,14 +1,43 @@
 package com.iyaselerehoboth.gadsleaderboard;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.iyaselerehoboth.gadsleaderboard.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        // In Activity's onCreate() for instance
+        // To make the notification bar transparent.
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        nextPage();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        nextPage();
+    }
+
+    public void nextPage() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Navigate to next page.
+                startActivity(new Intent(MainActivity.this, Homepage.class));
+            }
+        }, 2000);
     }
 }
